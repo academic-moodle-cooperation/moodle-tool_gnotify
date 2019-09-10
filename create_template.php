f501ea2e0be039f9e70e9426820694532062460a
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2019-09-10 18:54:18 +0200

    add template to database

--- create_template.php
+++ create_template.php
@@ -19,9 +19,30 @@ echo $OUTPUT->heading(get_string('createtemplate', 'tool_gnotify'));
 global $DB;
 
 $renderer = $PAGE->get_renderer('core');
-$cform = new tool_gnotify_create_form('create_template.php',[],'post');
-$params = ["cform" => $cform->render()];
+$cform = new tool_gnotify_create_form();
 
+if ($cform->is_cancelled()) {
+    redirect(new moodle_url('/admin/tool/gnotify/templates.php'));
+} else if ($fromform = $cform->get_data()) {
+
+    $trans = $DB->start_delegated_transaction();
+
+    $record = new stdClass();
+    $record->name = $fromform->template_name;
+    $id = $DB->insert_record('gnotify_tpl', $record, true);
+
+    $recordContent = new stdClass();
+    $recordContent->tplid = $id;
+    $recordContent->lang = 'en';
+    $recordContent->content = $fromform->content['text'];
+//TODO: lang should be not null
+    $DB->insert_record('gnotify_tpl_lang', $recordContent);
+
+    $DB->commit_delegated_transaction($trans);
+    redirect(new moodle_url('/admin/tool/gnotify/templates.php'));
+}
+
+$params = ["cform" => $cform->render()];
 
 echo $renderer->render_from_template('tool_gnotify/create_template', $params);
 
